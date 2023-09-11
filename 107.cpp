6d161a0daef034b9af958d5bc92d4cc2397a3417
Krish Vijay Ramani <136609165+KrishRamani2@users.noreply.github.com>
2023-09-11 10:16:14 +0530

    Add files via upload

---
+++ 107.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+void printGroups(int arr[], int n)
+{
+	for(int i = 1; i < n; i++)
+	{
+		if(arr[i] != arr[i - 1])
+		{
+			if(arr[i] != arr[0])
+                cout << "From " << i << " to ";
+			else
+                cout << i - 1 << endl;
+		}
+	}
+	if(arr[n - 1] != arr[0])
+        cout << n - 1 << endl;
+}
+int main() 
+{
+    int arr[] = {0,0,1,1,0,0,1,1,0,1}, n = 10;
+    printGroups(arr, n);
+}
